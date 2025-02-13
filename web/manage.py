89a26ab1570c1529051a91247e0946c257e17bab
Adam <safety5646@gmail.com>
2025-02-13 16:59:42 -0600

    Added navigation functionality.

--- web/manage.py
+++ web/manage.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, render_template_string
 import sys, os
 from cachetools import TTLCache
 
-max_users = 20
+max_users = 1
 sim_timeout = 120 #120 seconds
 
 #times out instances after some time
@@ -14,42 +14,53 @@ sims_cache = TTLCache(max_users, sim_timeout)
 app = Flask(__name__)
 
 
-#Template Fetchers
-
-@app.route("/")
-def page_load():
-    return redirect("/home")
-
-@app.route("/graph")
-def get_graph_template():
-    return render_template("main_graph.html")
-
-@app.route("/nav")
-def get_nav_template():
-    return render_template("nav.html")
-
-@app.route("/home")
-def home():
-    return render_template("home.html")
-
-
-    
-
-
-
-
-
+#Template Fetchers swap to general fetcher with database
 """
 @app.route("/templates/<path:filename>")
 def templates(filename):
     #for dynamically passing html templates via urlfor so they can get renderd with jinja as they are passed
     return render_template("graph.html")
 """
-@app.route("/simulation", methods=['GET'])
-def simulation():
-    return "<script>fetch(`${window.location.href}/sim_data`,{method :'post'})</script>"
 
-@app.route("/simulation/sim_data", methods=["POST"])
+
+def build_page(page_route_str):
+    template = render_template("background.html")
+
+    connection = "{" + "% include '{}.html' %".format(page_route_str) + "}"
+
+    print(connection, file=sys.stderr)
+
+    template = template.replace("%%", connection)
+    return template
+
+@app.route("/")
+def page_load():
+    return redirect("/home")
+
+@app.route("/nav")
+def get_nav_template():
+    return render_template("nav.html")
+
+@app.route("/simulation")
+def get_graph_template():
+    built_page = build_page("simulation")
+    return render_template_string(built_page)
+
+@app.route("/home")
+def home():
+    built_page = build_page("home")
+    return render_template_string(built_page)
+
+@app.route("/about")
+def about():
+    built_page = build_page("about")
+    return render_template_string(built_page)
+
+
+
+#Async js requests
+
+@app.route("/simulation/sim_data", methods=["GET"])
 def sim_data():
     js_file = ""
     print("./web/templates/dynamic/simulation_data.js")
--- web/static/nav/nav.js
+++ web/static/nav/nav.js
@@ -2,13 +2,26 @@ console.log("nav pulled");
 //window.onload(set_active_button())
 
 
-document.addEventListener("DOMContentLoaded", set_active_button)
+document.addEventListener("DOMContentLoaded", startup)
+
+
+function startup(){
+    set_active_button();
+
+}
 function set_active_button(){
     var button_dict = {};
     //get all available pages(buttons point directly to pages)
     const nav_buttons = document.getElementsByClassName("nav_button");
+
+    //maps buttons to
     Array.from(nav_buttons).forEach(button_elem => {
         let txt = button_elem.innerText.trim().toLowerCase();
+
+        button_elem.addEventListener('click', function(){
+            window.location.pathname = txt
+        })
+        
         button_dict[txt] = button_elem;
     });
 
@@ -16,9 +29,9 @@ function set_active_button(){
     const subURL = window.location.pathname.slice(1);
     var button = button_dict[subURL];
 
+    //adds onclick to 
     button.className+=" nav_pressed"
 
-    console.log(button, subURL, button_dict);
 
 }
 
